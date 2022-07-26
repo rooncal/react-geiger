11d34a52349ba9da169cfcf945d007e69532996b
רון קלקודה <roon@rwns-MacBook-Pro.local>
2022-07-26 14:58:44 +0300

    added more styling

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,9 +4,9 @@ const Info = () => {
     return (
         <div className="info--container">
             <img className="info--image" src="src/assets/pic.jpeg"/>
-            <h1>Geiger Geigerson</h1>
-            <h2>Professional Geiger</h2>
-            <h3>geiger.org</h3>
+            <h1 className="info--header">Geiger Geigerson</h1>
+            <h2 className="info--profession">Professional Geiger</h2>
+            <h3 className="info--link">geiger.org</h3>
             <div className="info--buttons">
                 <button className="email--button"><img src="src/assets/Email_icon.png" /> Email</button>
                 <button className="linkedin--button"><img src="src/assets/Linkedin_Icon.png" /> LinkedIn</button>
